51f2b22e95908e43c865fda40b21d1918caeddd9
mulkong <waterbottle2487@gmail.com>
2020-09-07 21:18:07 +0900

    Add data transforms [DataLoader.py]

--- CycleGAN_Pytorch/data_processing/DataLoader.py
+++ CycleGAN_Pytorch/data_processing/DataLoader.py
@@ -5,6 +5,7 @@ from torch.utils.data import Dataset
 import os
 import cv2
 import numpy as np
+from skimage.transform import resize
 
 
 class Dataset(Dataset):
@@ -89,7 +90,6 @@ class Dataset(Dataset):
 class ToTensor(object):
     def __call__(self, data):
         """
-
         :param data: type is dict ['img_A': ____, 'img_B': ____]
         :return: numpy of directory converted to tensor (data)
         """
@@ -98,6 +98,81 @@ class ToTensor(object):
             data[key] = torch.from_numpy(value)
 
         return data
+
+class Normalization(object):
+    def __init__(self, mean=0.5, std=0.5):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, data):
+        """
+        :param data: type is dict ['img_A': ____, 'img_B': ____]
+        :return: numpy of directory converted to tensor (data)
+        """
+        for key, value in data.items():
+            data[key] = (value - self.mean) / self.std
+
+        return data
+
+class Resize(object):
+    def __init__(self, shape):
+        self.shape = shape
+
+    def __call__(self, data):
+        """
+        :param data: type is dict ['img_A': ____, 'img_B': ____]
+        :return: numpy of directory converted to tensor (data)
+        """
+        for key, value in data.items():
+            data[key] = resize(image=value, output_shape=(self.shape[0], self.shape[1], self.shape[2]))
+
+        return data
+
+class RandomFlip(object):
+    def __call__(self, data):
+        """
+        :param data: type is dict ['img_A': ____, 'img_B': ____]
+        :return: numpy of directory converted to tensor (data)
+        """
+        if np.random.rand() > 0.5:
+            for key, value in data.items():
+                data[key] = np.flip(value, axis=0)
+        if np.random.rand() > 0.5:
+            for key, value in data.items():
+                data[key] = np.flip(value, axis=1)
+        return data
+
+class RandomCrop(object):
+    def __init__(self, shape):
+        self.shape = shape
+
+    def __call__(self, data):
+        """
+        :param data: type is dict ['img_A': ____, 'img_B': ____]
+        :return: numpy of directory converted to tensor (data)
+        """
+        keys = list(data.keys())
+
+        h, w = data[keys[0]].shape[:2]
+        new_h, new_w = self.shape
+
+        top = np.random.randint(0, h - new_h)
+        left = np.random.randint(0, w - new_w)
+
+        id_y = np.arange(top, top + new_h, 1)[:, np.newaxis]
+        id_x = np.arange(left, left + new_w, 1)
+
+        for key, value in data.items():
+            data[key] = value[id_y, id_x]
+
+        return data
+
+
+
+
+
+
+
 
 
 
