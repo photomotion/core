e3a68fa420f8b8eb4c3182405f8925efd05b572f
mulkong <waterbottle2487@gmail.com>
2020-09-12 17:44:30 +0900

    Add training utility [weight init, set requires grad]

---
+++ CycleGAN_Pytorch/utils.py
@@ -0,0 +1,37 @@
+import torch
+import torch.nn as nn
+
+import os
+
+
+def weights_init_normal(net, init_type='normal', init_gain=0.2):
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            if init_type == 'normal':
+                nn.init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                nn.init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                nn.init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            else:
+                raise NotImplementedError('initialization method [%s] is not implemented' % init_type)
+
+            if hasattr(m, 'bias') and m.bias is not None:
+                nn.init.constant_(m.bias.data, 0.0)
+        elif classname.find('BatchNorm2d') != -1:
+            nn.init.normal_(m.weight.data, 1.0, init_gain)
+            nn.init.constant_(m.bias.data, 0.0)
+
+    print('initialize network %s' % init_type)
+    net.apply(init_func)
+
+def set_requires_grad(nets, requires_grad=False):
+    if not isinstance(nets, list):
+        nets = [nets]
+    for net in nets:
+        if net is not None:
+            for param in net.parameters():
+                param.requires_grad = requires_grad
+
+
